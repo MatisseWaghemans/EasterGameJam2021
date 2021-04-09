ec25c3fb806bedaa7476ef146034db7b97953369
Matisse Waghemans <55841549+MatisseWaghemans@users.noreply.github.com>
2021-04-09 23:13:10 +0200

    Added temp personal files

---
+++ UnityProject_EasterGameJam2021/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+	[SerializeField]
+	private int _playerID; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		CheckControllerInput();
+		ShootBall();
+    }
+
+	private void CheckControllerInput()
+	{
+		if(Input.GetAxis("Horizontal"))
+		{
+
+		}
+	}
+
+	private void ShootBall()
+	{
+		throw new NotImplementedException();
+	}
+}
