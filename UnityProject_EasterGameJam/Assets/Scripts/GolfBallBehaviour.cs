2e4b21cbd47b6a9f12c4d2bc091913500af5f2c1
Matisse Waghemans <55841549+MatisseWaghemans@users.noreply.github.com>
2021-04-10 19:31:30 +0200

    Small changes to ball controller

--- UnityProject_EasterGameJam/Assets/Scripts/GolfBallBehaviour.cs
+++ UnityProject_EasterGameJam/Assets/Scripts/GolfBallBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,99 +7,130 @@ using UnityEngine.InputSystem;
 public class GolfBallBehaviour : MonoBehaviour
 {
 
-    [SerializeField] private Transform _cameraPivot;
+	[SerializeField] private Transform _cameraPivot;
 
-    [SerializeField] private float _aimSpeedDirection;
+	[SerializeField] private LineRenderer _lineRenderer;
 
-    private float _controllerStickValue;
-    private Vector3 _controllerStickVector;
+	[SerializeField] private float _aimSpeedDirection;
 
-    Gamepad _gamepad = Gamepad.current;
+	private float _controllerStickValue;
+	private Vector3 _controllerStickVector;
 
-    Vector3 _aimDirection;
+	Gamepad _gamepad = Gamepad.current;
 
-    private bool _ableToShoot = false;
+	Vector3 _aimDirection;
 
-    private bool _buttonPressed = false;
+	private bool _ableToShoot = false;
 
-    // Update is called once per frame
-    void Update()
-    {
+	private bool _buttonPressed = false;
 
-        //joystick direction 
+	private PlayerStates _currentPlayerState;
 
-        Vector3 forward = _cameraPivot.forward;
-        forward.y = 0;
-        forward.Normalize();
+	private PlayerStates _previousPlayerState;
 
-        Vector3 right = _cameraPivot.right;
-        right.y = 0;
-        right.Normalize();
+	private void Start()
+	{
+		_currentPlayerState = PlayerStates.Shooting;
+		_previousPlayerState = _currentPlayerState;
+	}
 
-        _aimDirection = (forward * _gamepad.leftStick.y.ReadValue() + right * _gamepad.leftStick.x.ReadValue());
-        if (_aimDirection.magnitude > 1)
-        {
-            _aimDirection.Normalize();
-        }
+	// Update is called once per frame
+	void Update()
+	{
+		switch (_currentPlayerState)
+		{
+			case PlayerStates.Paused:
+				break;
+			case PlayerStates.Shooting:
+				UpdatePlayerShootingControls();
+				break;
+			case PlayerStates.Spectating:
+				break;
+			case PlayerStates.Placing:
+				break;
+			default:
+				break;
+		}
 
-        if (_gamepad.leftStick.ReadValue() != Vector2.zero && this.transform.GetComponent<Rigidbody>().velocity == Vector3.zero)
-        {
-           // Debug.DrawLine(this.transform.position, this.transform.position + _aimDirection * _aimSpeedDirection);
-            _cameraPivot.GetComponent<LineRenderer>().SetPosition(0, this.transform.position);
-            _cameraPivot.GetComponent<LineRenderer>().SetPosition(1, this.transform.position + _aimDirection * _aimSpeedDirection);
-            //Debug.Log(_aimDirection);
-        }
+	}
 
-        if (_gamepad.buttonSouth.wasPressedThisFrame)
-        {
-            _buttonPressed = true;
-        }
-        if (_gamepad.buttonSouth.wasReleasedThisFrame)
-        {
-            _buttonPressed = false;
-        }
+	private void UpdatePlayerShootingControls()
+	{
+		//joystick direction 
 
-    }
-    private void FixedUpdate()
-    {
-        CheckVelocity(this.GetComponent<Rigidbody>());
+		Vector3 forward = _cameraPivot.forward;
+		forward.y = 0;
+		forward.Normalize();
 
-        //if (_gamepad.leftStick.ReadValue().magnitude < .01f && _controllerStickValue > .1f && this.transform.GetComponent<Rigidbody>().velocity == Vector3.zero)
-        //{
+		Vector3 right = _cameraPivot.right;
+		right.y = 0;
+		right.Normalize();
 
-        //   ShootGolfBall(_controllerStickVector * _aimSpeedDirection);
+		_aimDirection = (forward * _gamepad.leftStick.y.ReadValue() + right * _gamepad.leftStick.x.ReadValue());
+		if (_aimDirection.magnitude > 1)
+		{
+			_aimDirection.Normalize();
+		}
 
-        //}
-        Debug.Log(_buttonPressed);
-        if (_buttonPressed)
-        {
-            ShootGolfBall(_controllerStickVector * _aimSpeedDirection);
-            _buttonPressed = false;
-        }
+		if (_gamepad.leftStick.ReadValue() != Vector2.zero && this.transform.GetComponent<Rigidbody>().velocity == Vector3.zero)
+		{
+			// Debug.DrawLine(this.transform.position, this.transform.position + _aimDirection * _aimSpeedDirection);
+			_lineRenderer.SetPosition(0, this.transform.position);
+			_lineRenderer.SetPosition(1, this.transform.position + _aimDirection * _aimSpeedDirection);
+			//Debug.Log(_aimDirection);
+		}
 
-        _controllerStickValue = _gamepad.leftStick.ReadValue().magnitude;
-        _controllerStickVector = _aimDirection;
+		if (_gamepad.buttonSouth.wasPressedThisFrame)
+		{
+			_buttonPressed = true;
+		}
+		if (_gamepad.buttonSouth.wasReleasedThisFrame)
+		{
+			_buttonPressed = false;
+		}
+	}
 
-    }
+	private void FixedUpdate()
+	{
+		CheckVelocity(this.GetComponent<Rigidbody>());
 
-    private void ShootGolfBall(Vector3 shootingForce)
-    {
-        
-        Vector3 flatPlaneVector = new Vector3(shootingForce.x, .2f, shootingForce.z);
-        this.GetComponent<Rigidbody>().AddForce(flatPlaneVector * 10f, ForceMode.Impulse);
+		//if (_gamepad.leftStick.ReadValue().magnitude < .01f && _controllerStickValue > .1f && this.transform.GetComponent<Rigidbody>().velocity == Vector3.zero)
+		//{
 
-        _cameraPivot.GetComponent<LineRenderer>().enabled = false;
-    }
+		//   ShootGolfBall(_controllerStickVector * _aimSpeedDirection);
 
-    private void CheckVelocity(Rigidbody rb)
-    {
-        if(rb.velocity == Vector3.zero)
-        {
-            _cameraPivot.GetComponent<LineRenderer>().enabled = true;
-        }
-        else
-        {
-            _cameraPivot.GetComponent<LineRenderer>().enabled = false;
-        }
-    }
+		//}
+		Debug.Log(_buttonPressed);
+		if (_buttonPressed)
+		{
+			ShootGolfBall(_controllerStickVector * _aimSpeedDirection);
+			_buttonPressed = false;
+		}
+
+		_controllerStickValue = _gamepad.leftStick.ReadValue().magnitude;
+		_controllerStickVector = _aimDirection;
+
+	}
+
+	private void ShootGolfBall(Vector3 shootingForce)
+	{
+
+		Vector3 flatPlaneVector = new Vector3(shootingForce.x, .2f, shootingForce.z);
+		this.GetComponent<Rigidbody>().AddForce(flatPlaneVector * 10f, ForceMode.Impulse);
+
+		_lineRenderer.enabled = false;
+	}
+
+	private void CheckVelocity(Rigidbody rb)
+	{
+		if (rb.velocity == Vector3.zero)
+		{
+			_lineRenderer.enabled = true;
+		}
+		else
+		{
+			_lineRenderer.enabled = false;
+		}
+	}
 }
+
